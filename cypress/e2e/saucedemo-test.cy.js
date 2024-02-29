58f2cf85fee8a34ba18c314a80aea0d94682c402
Isaac-Sturtridge <eyes330@yahoo.co.uk>
2024-02-29 15:07:07 +0000

    tests for locked_out_user and problem_user

--- cypress/e2e/saucedemo-test.cy.js
+++ cypress/e2e/saucedemo-test.cy.js
@@ -1,21 +1,35 @@
-describe('tests for login', () => {
-    it('can access the sourcedemo site', () => {
-        cy.visit('https://www.saucedemo.com/')
-        cy.get("#user-name").type("standard_user")
-        cy.get("#password").type("secret_sauce", {log: false})
-        cy.get("#login-button").click()
-        cy.location().then((location) => {
-            cy.wrap(location.href).should(
-                "contain",
-                "inventory.html"
-            )
-        })
-    })
-    it('shows an error with invalid credentials', () => {
-        cy.visit("http://saucedemo.com")
-        cy.get("#user-name").type("wrong_username")
-        cy.get("#password").type("wrong_password", {log: false})
-        cy.get("#login-button").click()
-        cy.get(".error").should("be.visible")
+describe("tests for login", () => {
+  it("can access the sourcedemo site", () => {
+    cy.visit("https://www.saucedemo.com/");
+    cy.get("#user-name").type("standard_user");
+    cy.get("#password").type("secret_sauce", { log: false });
+    cy.get("#login-button").click();
+    cy.location().then((location) => {
+      cy.wrap(location.href).should("contain", "inventory.html");
     });
+  });
+  it("shows an error with invalid credentials", () => {
+    cy.visit("http://saucedemo.com");
+    cy.get("#user-name").type("wrong_username");
+    cy.get("#password").type("wrong_password", { log: false });
+    cy.get("#login-button").click();
+    cy.get(".error").should("be.visible");
+  });
+});
+
+describe("Advanced logins", () => {
+  it("detects error on the locked_out user", () => {
+    cy.visit("https://www.saucedemo.com/");
+    cy.get("#user-name").type("locked_out_user");
+    cy.get("#password").type("secret_sauce", { log: false });
+    cy.get("#login-button").click()
+    cy.get(".error").should("be.visible");
+  })
+  it('detects a problem user logging in', () => {
+    cy.visit("https://www.saucedemo.com/");
+    cy.get("#user-name").type("problem_user");
+    cy.get("#password").type("secret_sauce", { log: false });
+    cy.get("#login-button").click()
+    cy.get("img[src=\"/static/media/sl-404.168b1cce.jpg\"]").should("exist");
+  });
 });
