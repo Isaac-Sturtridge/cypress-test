99384853ccbbbebebee6729f4541df53d529b92b
Isaac-Sturtridge <eyes330@yahoo.co.uk>
2024-02-29 14:28:40 +0000

    added tests to login correctly and incorrectly to saucedemo

--- cypress.config.js
+++ cypress.config.js
@@ -7,4 +7,5 @@ module.exports = defineConfig({
     },
     baseUrl: 'http://localhost:5173'
   },
+  chromeWebSecurity: false
 });
--- cypress/e2e/saucedemo-test.cy.js
+++ cypress/e2e/saucedemo-test.cy.js
@@ -1,5 +1,21 @@
 describe('tests for login', () => {
     it('can access the sourcedemo site', () => {
-        cy.visit('https://www.google.com/')
+        cy.visit('https://www.saucedemo.com/')
+        cy.get("#user-name").type("standard_user")
+        cy.get("#password").type("secret_sauce", {log: false})
+        cy.get("#login-button").click()
+        cy.location().then((location) => {
+            cy.wrap(location.href).should(
+                "contain",
+                "inventory.html"
+            )
+        })
     })
+    it('shows an error with invalid credentials', () => {
+        cy.visit("http://saucedemo.com")
+        cy.get("#user-name").type("wrong_username")
+        cy.get("#password").type("wrong_password", {log: false})
+        cy.get("#login-button").click()
+        cy.get(".error").should("be.visible")
+    });
 });
